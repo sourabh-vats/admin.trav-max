8e0b6fc6e0df65928be5534b98b9e0c311a4e425
sourabhvatscoder <sourabhvats96@gmail.com>
2023-06-16 11:06:58 +0530

    dev

--- app/Models/Users_model.php
+++ app/Models/Users_model.php
@@ -8,6 +8,8 @@ class Users_model extends Model
     function validates($user_name, $password)
     {
         $db = \Config\Database::connect();
+        echo $user_name;
+        die();
         $query = $db->table('membership')
             ->select('*')
             ->where('user_name', $user_name)
