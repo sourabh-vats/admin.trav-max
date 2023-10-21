7bcd45ba80a93e7a190193bbd4ad685ba7499afc
sourabhvatscoder <sourabhvats96@gmail.com>
2023-10-21 09:49:36 +0530

    test

--- app/Views/includes/admin/header.php
+++ app/Views/includes/admin/header.php
@@ -6,12 +6,12 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<script src="<?php echo base_url(); ?>js/jquery-2.2.min.js"></script>
-	<script src="<?php echo base_url(); ?>bootstrap/js/bootstrap.min.js"></script>
+	<script src="/js/jquery-2.2.min.js"></script>
+	<script src="/bootstrap/js/bootstrap.min.js"></script>
 
 
-	<link href="<?php echo base_url(); ?>css/global-admin.css" rel="stylesheet" type="text/css">
-	<link href="<?php echo base_url(); ?>css/admin-style.css" rel="stylesheet" type="text/css">
+	<link href="/css/global-admin.css" rel="stylesheet" type="text/css">
+	<link href="/css/admin-style.css" rel="stylesheet" type="text/css">
   
 
   
